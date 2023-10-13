cc76712144c8a4ea2037b718e881f47212b99c02
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-10-13 11:49:10 +0300

    recording+ final touches
    recording the game in action

--- Assets/Scripts/WayPoints.cs
+++ Assets/Scripts/WayPoints.cs
@@ -11,7 +11,7 @@ public class WayPoints : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(transform.position);
+        Debug.Log(transform.position); 
     }
 
     // Update is called once per frame
