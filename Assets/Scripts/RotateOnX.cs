29a5edae06935aef212f3dfcd068557151ae5d9b
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-10-13 10:46:52 +0300

    moving boxes up and down
    Using C# to animate the boxes as obstacles to move up and down with a certain distance and speed.

--- Assets/Scripts/RotateOnX.cs
+++ Assets/Scripts/RotateOnX.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotatingOnY : MonoBehaviour
+public class RotateOnX : MonoBehaviour
 {
-    float rotationSpeed = 20f;
-    Vector3 newPosition = new Vector3(0f, 1f, 0f);
-
     // Start is called before the first frame update
+    float rotationSpeed = 20f;
+    Vector3 newposition = new Vector3(1f, 0f, 0f);
     void Start()
     {
         Debug.Log(transform.position);
@@ -16,6 +15,6 @@ public class RotatingOnY : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.right, rotationSpeed * Time.deltaTime);
     }
 }
